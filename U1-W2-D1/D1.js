304fb297a85b91367e734402ab2ca14e7e7b8e05
Alessandro Di Benedetto <socialorne02@gmail.com>
2025-02-10 14:52:58 +0100

    update exercise

--- U1-W2-D1/D1.js
+++ U1-W2-D1/D1.js
@@ -10,53 +10,44 @@ REGOLE
 /* ESERCIZIO 1
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
+
 /* SCRIVI QUI LA TUA RISPOSTA */
-console.log('In JS troviamo diversi datatype. Alcuni di questi sono ad esempio le string (stringhe di testo), i number (numeri), boolean (valore vero o falso), undefined (una variabile dichiarata ma non inizializzata) e null (non ha valore)');
 
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
+
 /* SCRIVI QUI LA TUA RISPOSTA */
-const myName = 'Alessandro'; 
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
+
 /* SCRIVI QUI LA TUA RISPOSTA */
- console.log(12+20) 
- //il alternativa
-const a = 12;
-const b = 20;
-const result = a+b; 
-console.log(result);
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
+
 /* SCRIVI QUI LA TUA RISPOSTA */
-let x = 12;
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
+
 /* SCRIVI QUI LA TUA RISPOSTA */
-// const myName = DiBenedetto;  Ho commentato la riga di codice altrimenti la console dava errore e non potevo proseguire.
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
+
 /* SCRIVI QUI LA TUA RISPOSTA */
-console.log(4-x);
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
  Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
-/* SCRIVI QUI LA TUA RISPOSTA */
 
-const name1 = 'john';
-const name2 = 'John';
-console.log(name1 === name2 );
-console.log(name1 !== name2);
+/* SCRIVI QUI LA TUA RISPOSTA */
